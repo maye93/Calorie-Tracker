aa2984775b28e0551a8483547e2c4f47ce5e9cb3
maye93 <71053941+maye93@users.noreply.github.com>
2022-02-08 18:16:04 +0800

    finished

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-// function BMI(){
-// 	var w=document.getElementById('w').value;
-// 	var h=document.getElementById('h').value;
-// 	var bmi=w/(h/100*h/100);
-// 	document.getElementById("result").innerHTML="Your BMI is" + bmi;
-// }
-
-// function BMI() {
-// 	var height=document.getElementById('height').value;
-// 	var weight=document.getElementById('weight').value;
-// 	var bmi=weight/(height/100*h/100);
-// 	var bmio=(bmi.toFixed(2));
-
-// 	document.getElementById("result").innerHTML="Your BMI is " + bmio;
-// }
-
 function validate(evt) {
 	var theEvent = evt || window.event;
-  
-	// Handle paste
 	if (theEvent.type === 'paste') {
 		key = event.clipboardData.getData('text/plain');
 	} else {
-	// Handle key press
 		var key = theEvent.keyCode || theEvent.which;
 		key = String.fromCharCode(key);
 	}
@@ -32,41 +13,24 @@ function validate(evt) {
 	}
 }
 
-function BMI() {
-  
-	const age = document.getElementById('age');
-	const gender = document.getElementById('gender').value;
-	const weight = document.getElementById('weight');
-	const height = document.getElementById('height');
-	const activity = document.getElementById('activity').value;
-	var bmi
-	// var bmio=(bmi.toFixed(2));
-	
-	if (age.value === '' || weight.value === '' || height.value === '' || 80 < age.value || age.value < 15) {
-	  errorMessage('Please enter the values.')
-	} else if(gender === 'male' && activity === "1") {
-	  bmi = 1.2 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-	} else if(gender === 'male' && activity === "2") {
-	  bmi = 1.375 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-	} else if (gender === 'male' && activity === "3") {
-	  bmi = 1.55 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-	} else if(gender === 'male' && activity === "4") {
-	  bmi = 1.725 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-	} else if(gender === 'male' && activity === "5") {
-	  bmi = 1.9 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)))
-	  ;
-	} else if(gender === 'female' && activity === "1") {
-	  bmi = 1.2 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-	} else if(gender === 'female' && activity === "2") {
-	  bmi = 1.375 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-	} else if(gender === 'female' && activity === "3") {
-	  bmi = 1.55 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-	} else if(gender === 'female' && activity === "4") {
-	  bmi = 1.725* (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-	} else {
-	  bmi = 1.9 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height)) - (4.676 * parseFloat(age.value)));
-	} 
-  
-	// document.getElementById('results').style.display = 'block';
-	document.getElementById("result").innerHTML="Your BMI is " + bmi;
-  }
+function calculate() {
+    const age = document.getElementById('age');
+    const gender = document.getElementById('gender').value;
+    const weight = document.getElementById('weight');
+    const height = document.getElementById('height');
+    const activity = document.getElementById('activity').value;
+    var cal = 0;
+    activityCal = [1.2,1.375, 1.55, 1.725, 1.9]
+    weightCal = [13.75, 9.563]
+    heightCal = [5.003, 1.850]
+    genderCal = [6.755, 4.676]
+    offset = [66.5, 655]
+    if(gender === 'male')
+      genderIndex = 0
+    else
+      genderIndex = 1
+
+    cal = activityCal[parseInt(activity) - 1] * (offset[genderIndex] + (weightCal[genderIndex] * parseFloat(weight.value)) + (heightCal[genderIndex] * parseFloat(height.value)) - (genderCal[genderIndex] * parseFloat(age.value)))
+    console.log(cal);
+    document.getElementById("result").innerHTML="Total Calories is " + cal.toFixed(2);
+}
